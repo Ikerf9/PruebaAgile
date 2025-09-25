8afadc1a762e96cefb86e3271305d512c3880288
Ikerf9 <farinas.iker@uni.eus>
2025-09-25 10:46:57 +0200

    aldaketak

---
+++ Ariketak/Ariketa1/kalkulagailua.py
@@ -0,0 +1,3 @@
+def batuketa(n1,n2):
+    emaitza=n1+n2
+    return emaitza
---
+++ Ariketak/Ariketa1/test.py
@@ -0,0 +1,8 @@
+import kalkulagailua
+if __name__ == '__main__':
+    input1=int(input("N1: "))
+    input2=int(input("N2: "))
+
+    
+    print(kalkulagailua.batuketa(input1, input2))
+
